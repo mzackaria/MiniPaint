6f31cfe6798bcb151d65ceb6feaef0881a598599
Zakaria MESSAI <messai.zackaria@gmail.com>
2022-01-06 11:33:21 +0100

    first commit

---
+++ app/src/main/java/com/example/minipaint/MyCanvasView2.kt
@@ -0,0 +1,97 @@
+package com.example.minipaint
+
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f
+
+class MyCanvasView2(context: Context) : View(context) {
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+    private var path = Path()
+    // Path representing the drawing so far
+    private val drawing = Path()
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private lateinit var frame: Rect
+
+    //le moment a partir on pense que l'utilisateur est en train de scroller
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        // Draw the drawing so far
+        canvas.drawPath(drawing, paint)
+// Draw any current squiggle
+        canvas.drawPath(path, paint)
+// Draw a frame around the canvas
+        canvas.drawRect(frame, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_UP -> touchEnd()
+            MotionEvent.ACTION_MOVE -> touchMove()
+        }
+        return true
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+        }
+        invalidate()
+    }
+
+    private fun touchEnd() {
+        drawing.addPath(path)
+        path.reset()
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+}
